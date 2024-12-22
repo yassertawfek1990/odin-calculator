c6f0e3768d0d478fb7c908fc6cd5bad45d2a2405
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-12-22 23:42:49 +0200

    done

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const add = (n1,n2)=>n1+n2
 const sub = (n1,n2)=>n1-n2
 const div = (n1,n2)=>{
     if(n2==0){
-        console.log("ERROR")
         return "ERROR"
     }
     else {
@@ -31,46 +30,56 @@ let n1 = ""
 let n2 = ""
 let sign
 let clear = false
+let signed=false
 const work=function (but,dis){
     
     if (but.textContent== "c"){dis.textContent=""}
     else if (signs.includes(but.textContent)){
-        if (n1 => 0){n2 = Number(dis.textContent)
+        if (signed == false){
+        if (n1){n2 = dis.textContent
+            n2 = Number(n2)
+            n1 = Number(n1)
             dis.textContent = operate(n1,sign,n2) % 1 != 0? parseFloat(operate(n1,sign,n2).toFixed(5)): operate(n1,sign,n2) == "ERROR"?  "ERROR" :operate(n1,sign,n2) 
             n1 = ""
             n2 = ""
             clear = true       
         }
-        n1 = Number(dis.textContent)
+        n1 = dis.textContent
         sign = but.textContent
         clear = true
-    }
+        signed = true
+    }else{n1 = dis.textContent
+        sign = but.textContent
+        clear = true
+        signed = true}}
     
-    else if (but.textContent== "="){n2 = Number(dis.textContent)
-        if (n1 => 0 && n2  ) {
-            console.log("hi")
+    else if (but.textContent== "="){n2 = dis.textContent
+        if (signed == false){
+        if (n1 && n2 ) {
+            n2 = Number(n2)
+            n1 = Number(n1)
         dis.textContent =  operate(n1,sign,n2) == "ERROR"?  "ERROR": operate(n1,sign,n2) % 1 == 0? operate(n1,sign,n2) : parseFloat(operate(n1,sign,n2).toFixed(5))
-        console.log("zero")
-        n1 = ""
+        n1 = dis.textContent
         n2 = ""
-        clear = true  
-        }
+        clear = true
+        signed = true
+        }}
         
     }
     else {
         if (clear){
             dis.textContent=""
             clear = false
+            signed = false
         }
 
-    dis.textContent += but.textContent
+    dis.textContent += !dis.textContent.includes(".")?but.textContent:but.textContent  != "."?but.textContent:""
     }
     but.blur();
 }
 
 
 window.addEventListener("keypress",(e)=>{
-    console.log(e)
     buts.forEach((but)=>{
     if (e.key == but.textContent){
         but.click()
@@ -78,11 +87,18 @@ window.addEventListener("keypress",(e)=>{
     else if (e.key == "Enter" && but.textContent == "="){
         but.click()
     }
+    else if (e.key == "*" && but.textContent == "x"){
+        but.click()
+    }
+   
 
 })})
-
+window.addEventListener("keydown",(e)=>{
+    if (e.key == "Backspace"){
+        di.textContent = di.textContent.substring(0,di.textContent.length-1)
+    }
+})
 buts.forEach((bu)=>{
-    console.log(bu.textContent)
     bu.onclick = () => work(bu, di)
 })
 
